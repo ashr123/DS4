89d29315df9e3cf235252befcda8daac7fe713d5
Roy <roykid123@gmail.com>
2017-05-27 16:07:40 +0300

    first Commit

---
+++ src/BTree.java
@@ -0,0 +1,99 @@
+// SUBMIT
+public class BTree implements BTreeInterface
+{
+	private final int t;
+	// ///////////////////BEGIN DO NOT CHANGE ///////////////////
+	// ///////////////////BEGIN DO NOT CHANGE ///////////////////
+	// ///////////////////BEGIN DO NOT CHANGE ///////////////////
+	private BNode root;
+	
+	/**
+	 * Construct an empty tree.
+	 */
+	public BTree(int t)
+	{
+		this.t=t;
+		//root=null;
+	}
+	
+	// For testing purposes.
+	public BTree(int t, BNode root)
+	{
+		this(t);
+		this.root=root;
+	}
+	
+	@Override
+	public BNode getRoot()
+	{
+		return root;
+	}
+	
+	@Override
+	public int getT()
+	{
+		return t;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		final int prime=31;
+		int result=1;
+		result=prime*result+(root==null ? 0 : root.hashCode());
+		result=prime*result+t;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this==obj)
+			return true;
+		if (obj==null)
+			return false;
+		if (getClass()!=obj.getClass())
+			return false;
+		BTree other=(BTree)obj;
+		if (root==null)
+		{
+			if (other.root!=null)
+				return false;
+		}
+		else
+			if (!root.equals(other.root))
+				return false;
+		return t==other.t;
+	}
+	// ///////////////////DO NOT CHANGE END///////////////////
+	// ///////////////////DO NOT CHANGE END///////////////////
+	// ///////////////////DO NOT CHANGE END///////////////////
+	
+	@Override
+	public Block search(int key)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void insert(Block b)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void delete(int key)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public MerkleBNode createMBT()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/HashUtils.java
@@ -0,0 +1,38 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+//Changed!!!
+/// DO NOT CHANGE
+/// DO NOT SUBMIT
+public class HashUtils
+{
+	/**
+	 * Concatenates the given data and hashes it with SHA1
+	 * @param dataList an ArrayList of byte arrays (byte[]) containing the data to be concatenated and hashed.
+	 * @return 20 byte fixed-length hash value.
+	 */
+	public static byte[] sha1Hash(ArrayList<byte[]> dataList)
+	{
+		MessageDigest crypt=null;
+		try
+		{
+			crypt=MessageDigest.getInstance("SHA-1");
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+		if (crypt != null)
+		{
+			crypt.reset();
+		}
+		for (byte[] data : dataList)
+		{
+			if (crypt != null)
+			{
+				crypt.update(data);
+			}
+		}
+		return crypt!=null ? crypt.digest() : new byte[0];
+	}
+}
